0d1c8b4981c067c16505560cc9febbaa709b21c3
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-07-24 21:19:39 -0300

    primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+import Header from '../componentes/header'
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Header/>
+    </main>
+  );
+}
---
+++ src/componentes/header.jsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import styles from '../style/header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'; 
+
+const Header = () => {
+    return (
+        <header className={styles.heade}>
+            <div>
+                <Image
+                    src='/suples.png'
+                    width={300} 
+                    height={150}
+                    alt='Logo image'
+                />
+            </div>
+            <div className={styles.altur}>
+                <input className={styles.inpu} type="search" />
+            </div>
+            <nav className={styles.estencao}>
+                <ul className={styles.opcoes}>
+                    <li>
+                        <div className={styles.alinha}>
+                            <p className={styles.noTem}>Não tem conta?</p>
+                            <a className={styles.cadastre} href="#">Cadastre-se</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a className={styles.opc} href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a className={styles.opc} href="#">Contatos</a>
+                    </li>
+                    <li>
+                        <FontAwesomeIcon
+                            icon={faCartShopping}
+                            style={{ color: "#ff8b00", width: 25, cursor: 'pointer' }}
+                            aria-label="Carrinho de compras"
+                        />
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
