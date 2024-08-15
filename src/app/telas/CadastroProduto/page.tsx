b428cfeb08100f01d98bb4bee8a79f30fa4cb6a5
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-08-15 19:06:02 -0300

    create adm

---
+++ src/app/telas/CadastroProduto/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/componentes/header"
+
+export default function CadastroProd(){
+    return(
+        <>
+        <Header/>
+        
+        </>
+    )
+}
---
+++ src/app/telas/principalAdm/page.jsx
@@ -0,0 +1,56 @@
+"use client"
+import Header from '../../../componentes/header'
+import styles from './style1.module.css'
+import {openSans} from '../../fonts';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import Popular from '@/componentes/popularProdutos';
+
+export default function MainAdm() {
+  
+  return (
+    <main className={styles.container}> 
+    <header className={styles.heder}>
+       <Header/>
+    </header>
+    <nav className={styles.opcoes}>
+      <ul className={`${openSans.className}  ${styles.alinhas}`}>
+        <li>
+          <a className={styles.pointer}>Whey</a>
+        </li>
+        <li>
+          <a className={styles.pointer}>Creatina</a>
+        </li>
+        <li>
+          <a className={styles.pointer}>Pré-treino</a>
+        </li>
+        <li>
+          <a className={styles.pointer}>Promoções</a>
+        </li>
+      </ul>
+    </nav>
+    <div className={styles.slide}>
+          <Splide 
+            className={styles.ca} 
+            aria-label="My Favorite Images"
+            options={{
+              type:'loop',
+              autoplay:true,
+              interval:3000,
+              arrows:false,
+            }}
+            >
+              <SplideSlide className={styles.imag1}>
+              </SplideSlide>
+              <SplideSlide className={styles.imag2}>
+              </SplideSlide>
+        </Splide>
+    </div>
+    <div className={styles.linha}></div>
+    <div className={styles.produtos}>
+          <Popular/>
+    </div>
+    <div className={styles.acre}></div>
+    </main>
+  );
+}
