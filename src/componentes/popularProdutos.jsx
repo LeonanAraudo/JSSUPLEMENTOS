b1cc764b56533ee7c0c85991008686f1ea0c178b
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-08-14 23:20:14 -0300

    popular fixed

--- src/componentes/popularProdutos.jsx
+++ src/componentes/popularProdutos.jsx
@@ -7,35 +7,46 @@ export default function Popular(){
     return(
     <div className={styles.spaceSlide}>
         <Splide
+        className={styles.slidesCon}
             options={{
               perPage: 5,
               perMove: 1,
-              rewind:true
+              rewind:true,
+              pagination:false,
+              fixedWidth:245,
+              gap:10
+              
             }}
             >
+              <SplideSlide  className={styles.slidesInicio}>
+                <Card/>
+              </SplideSlide>
               <SplideSlide >
                 <Card/>
               </SplideSlide>
               <SplideSlide >
-              <Card/>
+                <Card/>
               </SplideSlide>
               <SplideSlide >
-              <Card/>
+                <Card/>
               </SplideSlide>
               <SplideSlide >
-              <Card/>
+                <Card/>
               </SplideSlide>
               <SplideSlide >
-              <Card/>
+                 <Card/>
               </SplideSlide>
               <SplideSlide >
-              <Card/>
+                <Card/>
               </SplideSlide>
               <SplideSlide >
-              <Card/>
+                <Card/>
               </SplideSlide>
               <SplideSlide >
-              <Card/>
+                <Card/>
+              </SplideSlide>
+              <SplideSlide className={styles.slidesFim} >
+                <Card/>
               </SplideSlide>
         </Splide>
     </div>
