f6b852c55b9c892d348ad6483ef725b44954978e
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-08-18 22:58:19 -0300

    create desingForm

--- src/app/telas/CadastroProduto/page.tsx
+++ src/app/telas/CadastroProduto/page.tsx
@@ -1,9 +1,28 @@
-import Header from "@/componentes/headerAdm"
+import Header from "@/componentes/headerAdm";
+import styles from "./cadProd.module.css";
+import { montserrat } from '../../fonts';
 
 export default function CadastroProd(){
     return(
-        <>
-        <Header/>
-        </>
+        <div className={`${montserrat.className} ${styles.container}`}>
+            <header>
+                 <Header/>
+            </header>
+
+            <div className={styles.title}>
+                <h2>Cadastrar Produto</h2>
+            </div>
+
+            <form className={styles.forme}>
+                <div className={styles.primaMetade}>
+                    <input className={styles.selectImg} type="file"/>
+                </div>
+                <div className={styles.segunMetade}>
+
+                </div>
+            </form>
+
+        
+        </div>
     )
 }
