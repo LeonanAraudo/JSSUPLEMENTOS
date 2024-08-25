da724a0ded1c8ced8ae84314ffa4fb5443a60c1a
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-08-25 01:49:05 -0300

    slides ok

--- src/componentes/popularProdutos.jsx
+++ src/componentes/popularProdutos.jsx
@@ -19,14 +19,13 @@ export default function Popular(){
 
     fetchPosts();
   }, []);
-  console.log(posts)
     return(
         <div className={styles.spaceSlide}>
             <Splide
             className={styles.slidesCon}
                 options={{
                   perPage: 5,
-                  perMove: 2,
+                  perMove: 1,
                   rewind:true,
                   pagination:false,
                   fixedWidth:245,
@@ -35,17 +34,17 @@ export default function Popular(){
                 }}
                 >
                   {posts.map((post) => 
-                  <Link key={post.Produto_id} href={"/telas/login"}>          
-                    <SplideSlide className={styles.cards}>
-                      <Card  produto={{
-                          Nome: post.Nome,
-                          Preco: post.Preco,
-                          Preco_Antes: post.Preco_Antes,
-                          descricaoImagem: 'Descrição da imagem' 
-                          }} 
-                      />
+                    <SplideSlide key={post.Produto_id}>
+                      <Link href={"/telas/login"}>          
+                          <Card  produto={{
+                              Nome: post.Nome,
+                              Preco: post.Preco,
+                              Preco_Antes: post.Preco_Antes,
+                              descricaoImagem: 'Descrição da imagem' 
+                              }} 
+                          />
+                      </Link>
                     </SplideSlide>
-                  </Link>
                   )}
             </Splide>
         </div>
