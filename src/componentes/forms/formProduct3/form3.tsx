67316e57d10aafaa420db5aa8611e130f4daa329
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-10-17 16:23:04 -0300

    create Form3

--- src/componentes/forms/formProduct3/form3.tsx
+++ src/componentes/forms/formProduct3/form3.tsx
@@ -1,16 +1,18 @@
 import { Box, TextField } from "@mui/material";
-
+import styles from '../../../app/telas/CadastroProduto/cadProd.module.css'
 export default function Form3(){
     return(
-        <form>
-            <Box>
+        <form className={styles.ultimoForm}>
+            <Box sx={{width:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
             <TextField
+               sx={{ m: 1, width: '80ch'}}
                 id="outlined-multiline-static"
                 label="Componentes do Produto"
                 multiline
                 rows={7}
                 />
                 <TextField
+                sx={{ m: 1, width: '80ch'}}
                 id="outlined-multiline-static"
                 label="Modo de Usar"
                 multiline
