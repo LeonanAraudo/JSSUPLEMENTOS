6323fb2d6951fcf81d38e884ba35176b4967dbd2
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-11-07 16:39:55 -0300

    create getCliente

--- pages/api/getCliente/buscarCliente.js
+++ pages/api/getCliente/buscarCliente.js
@@ -1,5 +1,20 @@
-
+import usuario from '../../../models/usuario'
 
 export default async function buscarClientes(req,res){
-    
+    if(req.method === "GET"){
+        try{
+            const reqAllUsers = await usuario.findAll()
+            if(reqAllUsers){
+                res.status(200).json(reqAllUsers)
+            }else{
+                res.status(401).json("usuário não encotrado")
+            }
+        }catch(error){
+            res.status(500).json({error: "erro ao buscar o usuário"})
+        }
+    }
+    else{
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Método ${req.method} não permitido`)
+    }
 }
