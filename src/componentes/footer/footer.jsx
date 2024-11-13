646134a210c5e48ce97f1a4ec4417f418bf7f3a7
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-11-12 21:58:43 -0300

    update footer

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -3,8 +3,35 @@ export default function Footer(){
         <>
         <footer className="h-80 bg-black mt-[70px]">
         <div>
-            <div></div>
-            <div></div>
+            <div>
+                <p>Contatos</p>
+                 <a>Instagram</a>
+                 <a>Whatsapp</a>
+            </div>
+
+            <div>
+                <a>Ajuda</a>
+                <div>
+                    <a>Produtos</a>
+                    <a>Pagamentos</a>
+                    <a>Carrinho</a>
+                    <a>Entregas</a>
+                    <a>Fale conosco</a>
+                    <a>Pedidos</a>
+                    <a>Trocas e Devoluções</a>
+                </div>
+            </div>
+            <div>
+                <p>Forma de Pagamentos</p>
+            </div>
+        </div>
+        {/* <div>linha</div> */}
+        <div>
+            <div>
+                <p>Brasil</p>
+                <a>Politica de Privacidade</a>
+                <a>Termos de uso</a>
+            </div>
         </div>
         </footer>
         </>
